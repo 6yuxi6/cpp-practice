1684f7531655a9d98996e0b316f6fddc9994a7d5
6yuxi6 <102651240+6yuxi6@users.noreply.github.com>
2025-10-20 16:00:43 +0800

    Add pattern printer functionality in C++
    Implement a pattern printer that takes user input for rows, columns, and a symbol to print a pattern.

---
+++ pattern_printer/pattern_printer.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include<cmath>
+#include<string>
+
+int main()
+{
+
+	int rows;
+	int columns;
+	char symbol;
+
+	std::cout << "how many rows?:";
+	std::cin >> rows;
+	
+	std::cout << "how many columns?:";
+	std::cin >> columns;
+
+	std::cout << "enter a sybol to use:";
+	std::cin >> symbol;
+
+	for (int i = 1;i <= rows;i++) {
+		for (int j = 1;j <= columns;j++) {
+			std::cout << symbol;
+		}
+
+		std::cout << '\n';
+	}
+
+	return 0;
+
+}#include <iostream>
+#include<cmath>
+#include<string>
+
+int main()
+{
+
+	int rows;
+	int columns;
+	char symbol;
+
+	std::cout << "how many rows?:";
+	std::cin >> rows;
+	
+	std::cout << "how many columns?:";
+	std::cin >> columns;
+
+	std::cout << "enter a sybol to use:";
+	std::cin >> symbol;
+
+	for (int i = 1;i <= rows;i++) {
+		for (int j = 1;j <= columns;j++) {
+			std::cout << symbol;
+		}
+
+		std::cout << '\n';
+	}
+
+	return 0;
+
+}
